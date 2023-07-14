0af05ec0f99bf3631f62fa4eb174858ce87cfef4
Arnau Mora <arnyminerz@proton.me>
2023-07-14 17:03:18 +0200

    Added publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,3 +25,33 @@ tasks.test {
 kotlin {
     jvmToolchain(8)
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.arnyminerz.filamagenta"
+            artifactId = "commons"
+            version = "1.0.0"
+
+            from(components["java"])
+
+            pom {
+                name.set("Filà Magenta Commons")
+                description.set("The main library for all Kotlin projects. Includes all common functions and classes for client and server.")
+                url.set("https://github.com/FilaMagenta/Commons")
+                developers {
+                    developer {
+                        id.set("arnyminerz")
+                        name.set("Arnau Mora")
+                        email.set("arnyminerz@proton.me")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/FilaMagenta/Commons.git")
+                    developerConnection.set("scm:git:ssh://github.com/FilaMagenta/Commons.git")
+                    url.set("http://github.com/FilaMagenta/Commons")
+                }
+            }
+        }
+    }
+}
