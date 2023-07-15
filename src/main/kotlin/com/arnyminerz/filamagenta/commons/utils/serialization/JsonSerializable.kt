efb4f6abed737b1954c3799175df7ca44dce3925
Arnau Mora <arnyminerz@proton.me>
2023-07-15 10:29:16 +0200

    Added comments
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/serialization/JsonSerializable.kt
+++ src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/serialization/JsonSerializable.kt
@@ -1,7 +1,20 @@
 package com.arnyminerz.filamagenta.commons.utils.serialization
 
+import org.json.JSONException
 import org.json.JSONObject
 
+/**
+ * Indicates that the parent object can be converted into a [JSONObject]. Needs to implement [toJSON].
+ *
+ * @see JsonSerializer
+ */
 interface JsonSerializable {
+    /**
+     * Converts the current class into a [JSONObject].
+     *
+     * @return A [JSONObject] with all the data of the class.
+     *
+     * @throws [JSONException] If some field cannot be serialized into JSON.
+     */
     fun toJSON(): JSONObject
 }
--- src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/serialization/JsonSerializer.kt
+++ src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/serialization/JsonSerializer.kt
@@ -1,7 +1,22 @@
 package com.arnyminerz.filamagenta.commons.utils.serialization
 
+import org.json.JSONException
 import org.json.JSONObject
 
+/**
+ * Usually implemented in companion objects. Provides a way to convert a [JSONObject] into [Result]. May be used
+ * together with [JsonSerializable], for serializing and deserializing a data class.
+ *
+ * @param Result The resulting data type. Usually a [JsonSerializable].
+ */
 interface JsonSerializer<Result> {
+    /**
+     * Takes the data contained in [json], and converts into a [Result].
+     *
+     * @return The deserialized [Result] from the data in [json].
+     *
+     * @throws JSONException If a field is not valid or cannot be serialized.
+     * @throws NullPointerException If a field was expected but is not present in [json].
+     */
     suspend fun fromJson(json: JSONObject): Result
 }
