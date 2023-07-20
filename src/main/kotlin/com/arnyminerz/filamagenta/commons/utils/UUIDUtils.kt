d2d480be7d73c6d57ab557b4eaed679eca63a446
Arnau Mora <arnyminerz@proton.me>
2023-07-20 16:05:41 +0200

    Deprecated UUID Base64 encoding in favor of ByteArray
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/UUIDUtils.kt
+++ src/main/kotlin/com/arnyminerz/filamagenta/commons/utils/UUIDUtils.kt
@@ -1,15 +1,40 @@
 package com.arnyminerz.filamagenta.commons.utils
 
-import java.util.Base64
+import java.nio.ByteBuffer
 import java.util.UUID
 
 /**
- * Encodes the UUID as a base64 encoded byte array.
+ * The size of a UUID in bytes.
  *
- * @return The base64 encoded byte array representation of the UUID.
+ * The UUID_SIZE constant represents the number of bytes required to store a universally unique identifier (UUID).
+ * A UUID is a 128-bit value typically represented as a sequence of 32 hexadecimal digits separated by hyphens.
+ * This constant defines the size of the binary representation of a UUID, which is fixed at 16 bytes.
+ *
+ * @since 1.0.0
  */
-fun UUID.base64Encode(): ByteArray {
-    val encoder = Base64.getEncoder()
-    val bytes = toString().toByteArray()
-    return encoder.encode(bytes)
+private const val UUID_SIZE = 16
+
+/**
+ * Converts the UUID to a byte array representation.
+ *
+ * @return The byte array representation of the UUID.
+ */
+val UUID.bytes: ByteArray
+    get() {
+        val bb = ByteBuffer.wrap(ByteArray(UUID_SIZE))
+        bb.putLong(mostSignificantBits)
+        bb.putLong(leastSignificantBits)
+        return bb.array()
+    }
+
+/**
+ * Converts a byte array to a UUID.
+ *
+ * @return the UUID representation of the byte array
+ */
+fun ByteArray.toUUID(): UUID {
+    val bb = ByteBuffer.wrap(this)
+    val msb = bb.getLong()
+    val lsb = bb.getLong()
+    return UUID(msb, lsb)
 }
--- src/test/kotlin/utils/TestUUIDUtils.kt
+++ src/test/kotlin/utils/TestUUIDUtils.kt
@@ -1,19 +1,28 @@
 package utils
 
-import com.arnyminerz.filamagenta.commons.utils.base64Encode
-import java.util.Base64
+import com.arnyminerz.filamagenta.commons.utils.bytes
+import com.arnyminerz.filamagenta.commons.utils.toUUID
 import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class TestUUIDUtils {
     @Test
-    fun `test UUID to Base64 encoding`() {
+    fun `test UUID to ByteArray`() {
         val uuid = UUID.randomUUID()
-        val base64 = uuid.base64Encode()
+        val uuidBytes = uuid.bytes
+        val uuidBack = uuidBytes.toUUID()
 
-        val decoded = Base64.getDecoder().decode(base64).decodeToString()
+        assertEquals(uuid, uuidBack)
+    }
 
-        assertEquals(uuid.toString(), decoded)
+    @Test
+    fun `test UUID native method doesn't recreate original UUID`() {
+        val u = UUID.randomUUID()
+        val uBytes: ByteArray = u.bytes
+        val u2 = UUID.nameUUIDFromBytes(uBytes)
+
+        assertNotEquals(u, u2)
     }
 }
